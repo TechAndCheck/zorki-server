e44200da5ca1de931a8a1a39fe36227bb8de48a6
Christopher Guess <cguess@gmail.com>
2021-10-19 16:59:51 -0400

    Fix testing on M1 ARM processors and probably more
    This does two things to fix testinng:
    1. Remove Spring becuase it's always screwed up anyways and just throws
       errors, especially if you're using Postgres 14
    2. Remove the parallization of testing. This is because Chromehelper
       only allows one controller at a time, so if we don't have it they all
       start yelling at each other and it crashes out eventually.
    
    To test:
    `rails test` should run everything smoothly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,8 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # Do not run tests in parallel, since we're using a single Chromehelper instance parallelizing will screw everything up.
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
