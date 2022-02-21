c048356bb5856dd56a94f45d7fd077ddeb6c1617
oneroyalace <oneroyalace@gmail.com>
2022-02-21 17:13:01 -0500

    add Facebook BluePrints

---
+++ app/blueprints/facebook_post_blueprint.rb
@@ -0,0 +1,37 @@
+class FacebookPostBlueprint < Blueprinter::Base
+  identifier :url
+
+  fields  :url,
+          :text,
+          :has_video,
+          :created_at,
+          :num_comments,
+          :num_views,
+          :num_shares,
+          :reactions,
+          :id
+
+  association :user, blueprint: UserBlueprint
+
+  field :image_file do |post|
+    unless post.image_file.nil?
+      file = File.open(post.image_file).read
+      Base64.encode64(file)
+    end
+  end
+
+  field :video_file do |post|
+    unless post.video_file.nil?
+      file = File.open(post.video_file).read
+      Base64.encode64(file)
+    end
+  end
+
+  field :video_preview_image_file do |post|
+    unless post.video_preview_image_file.nil?
+      file = File.open(post.video_preview_image_file).read
+      Base64.encode64(file)
+    end
+  end
+end
+
