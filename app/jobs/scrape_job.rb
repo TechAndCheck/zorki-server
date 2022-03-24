0b53a496ee3cf4cb3044118f03b88f84b5f780d8
Christopher Guess <cguess@gmail.com>
2022-03-24 12:50:36 -0400

    Fix callback scraping issue

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -4,15 +4,15 @@ class ScrapeJob < ApplicationJob
   def perform(url, callback_id = nil, callback_url = nil)
     # If there's no callback id or the callback url isn't set, then ignore this
     # Otherwise, send it back to the source
-    return if callback_id.blank? || (Figaro.env.ZENODOTUS_URL.blank? && Figaro.env.callback_url.blank?)
+    return if callback_id.blank? || (Figaro.env.ZENODOTUS_URL.blank? && callback_url.blank?)
 
     results = MediaSource.scrape!(url, callback_id, callback_url)
 
-    print "Finished scraping #{url}"
+    print "\nFinished scraping #{url}\n"
 
-    print "********************"
+    print "\n********************\n"
     print "Sending callback to #{callback_url}"
-    print "********************"
+    print "\n********************\n"
     Typhoeus.post("#{callback_url}/scrape/callback.json",
         headers: { "Content-Type": "application/json" },
         body: { callback_id: callback_id, scrape_result: PostBlueprint.render(results) }
