00c218f73765a183383808d8b8ba8dbfba33eff4
Christopher Guess <cguess@gmail.com>
2022-06-09 00:17:16 -0400

    Update zorki and forki
    This brings news versions of Zorki and Forki in to fix a bunch of race
    conditions.

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -22,7 +22,8 @@ class ScraperController < ApplicationController
       render json: { error: "Url must be a proper #{ApplicationController.name_for_differentiated_type} url" }, status: 400
       return
     rescue Net::ReadTimeout => error
-      print({ error: "Net::ReadTimeout encountered whle scraping", url: url, count: retry_count += 1 }.to_json)
+      print({ error: "Net::ReadTimeout encountered while scraping", url: url, count: retry_count += 1 }.to_json)
+      sleep(30)
       raise error if retry_count > 4
       retry
     end
