7062cde39bd7b7b94fc797826ef229ec57a010c1
oneroyalace <oneroyalace@gmail.com>
2022-08-03 16:40:58 -0400

    Move Capybara/Selenium setup to an intializer file

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,19 +1,23 @@
 require "capybara/dsl"
 require "selenium-webdriver"
 
-options = Selenium::WebDriver::Chrome::Options.new
+# For screenshotting we're using Firefox instead of Chrome. This is because Chrome
+# cannot take full page screenshots.
+options = Selenium::WebDriver::Firefox::Options.new
 options.add_argument("--window-size=1400,1400")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--user-data-dir=/tmp/tarun")
 
-Capybara.register_driver :chrome do |app|
+# Here we assume we're using the same locally running scraping server that the gems would
+# be set to. This should be configurable if we ever get bigger
+Capybara.register_driver :firefox_hypatia do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
-  client.read_timeout = 10
-  Capybara::Selenium::Driver.new(app, browser: :chrome, url: "http://localhost:4444/wd/hub", capabilities: options, http_client: client)
+  client.read_timeout = 60  # Don't wait 60 seconds to return Net::ReadTimeoutError. We'll retry through Hypatia after 10 seconds
+  Capybara::Selenium::Driver.new(app, browser: :firefox, url: "http://localhost:4444/wd/hub", capabilities: options, http_client: client)
 end
 
-Capybara.default_max_wait_time = 7
 Capybara.threadsafe = true
+Capybara.default_max_wait_time = 60
 Capybara.reuse_server = true
-Capybara.default_driver = :chrome
+Capybara.default_driver = :firefox_hypatia
