a416905d461e568ece819327de0e6ce420cdfccb
Christopher Guess <cguess@gmail.com>
2022-10-20 13:52:06 -0400

    Require ZENODOTUS_API

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -4,12 +4,7 @@ Figaro.require_keys("SECRET_KEY_BASE")
 Figaro.require_keys("INSTAGRAM_USER_NAME", "INSTAGRAM_PASSWORD")
 Figaro.require_keys("FACEBOOK_EMAIL", "FACEBOOK_PASSWORD")
 Figaro.require_keys("TWITTER_BEARER_TOKEN")
-
-# We default to requiring ZENODOTUS_URL as a callback unless we explicitly set it otherwise.
-# Note: ZENODOTUS_URL can still be set, which will become the fallback if there's not a callback url passed in
-unless Figaro.env.ALLOW_CUSTOM_CALLBACK.blank? == false && Figaro.env.ALLOW_CUSTOM_CALLBACK == "true"
-  Figaro.require_keys("ZENODOTUS_URL")
-end
+Figaro.require_keys("ZENODOTUS_URL")
 
 # Hypatia can be configured two ways:
 # 1. Send anything scraped directly to a Zenodotus instance (or anything conforming to the API)
