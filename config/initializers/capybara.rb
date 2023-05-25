d0e95af6946f9b9e4c3b7bad0eb46a4f9573629e
Christopher Guess <cguess@gmail.com>
2023-05-25 13:15:06 -0400

    Update screen grab scraper to new syntax

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -5,17 +5,22 @@ require "selenium-webdriver"
 # cannot take full page screenshots.
 # options = Selenium::WebDriver::Firefox::Options.new
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument("--window-size=1200,3000")
+options.add_argument("--start-maximized")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--user-data-dir=/tmp/tarun_hypatia_#{SecureRandom.uuid}")
+options.add_argument("–-disable-blink-features=AutomationControlled")
+options.add_argument("--disable-extensions")
+options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+options.add_preference "password_manager_enabled", false
+options.add_argument("--user-data-dir=/tmp/tarun_zorki_#{SecureRandom.uuid}")
 
 # Here we assume we're using the same locally running scraping server that the gems would
 # be set to. This should be configurable if we ever get bigger
 Capybara.register_driver :hypatia do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.read_timeout = 60  # Don't wait 60 seconds to return Net::ReadTimeoutError. We'll retry through Hypatia after 10 seconds
-  Capybara::Selenium::Driver.new(app, browser: :chrome, url: "http://localhost:4444/wd/hub", capabilities: options, http_client: client)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
 end
 
 Capybara.threadsafe = true
