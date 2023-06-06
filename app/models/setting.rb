bc84fd3e7b665e20d37b09ec3aa08fd28e7f35d3
Christopher Guess <cguess@gmail.com>
2023-06-06 12:50:03 -0400

    Add ability to load custom API Key

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,7 @@
 class Setting < RailsSettings::Base
   cache_prefix { "v1" }
 
-  field :auth_key, type: :string, default: nil
+  field :auth_key, type: :string, default: Figaro.env.PRESET_API_KEY
 
   def self.generate_auth_key
     charset = Array("A".."Z") + Array("a".."z")
