83d56b16b3bd683b465055f2314f41ca39c723fd
Christopher Guess <cguess@gmail.com>
2023-08-01 13:11:07 -0400

    We now use Selenium Manager to make sure we don't have to deal with versioning

--- config/initializers/selenium.rb
+++ config/initializers/selenium.rb
@@ -1,12 +1,12 @@
 # We use the `webdrivers` gem to install and manage the web drivers for us. However, this doesn't
 # support Linux on ARM64, but Chromium installs it by default, so we just use that. Otherwise we
 # enable the gem.
-if RUBY_PLATFORM == "aarch64-linux"
-  unless File.file?("/usr/bin/chromedriver")
-    raise "Chromedriver missing, make sure you have Chromium installed"
-  end
+# if RUBY_PLATFORM == "aarch64-linux"
+#   unless File.file?("/usr/bin/chromedriver")
+#     raise "Chromedriver missing, make sure you have Chromium installed"
+#   end
 
-  Selenium::WebDriver::Chrome::Service.driver_path = "/usr/bin/chromedriver"
-else
-  require "webdrivers/chromedriver"
-end
+#   Selenium::WebDriver::Chrome::Service.driver_path = "/usr/bin/chromedriver"
+# else
+#   require "webdrivers/chromedriver"
+# end
