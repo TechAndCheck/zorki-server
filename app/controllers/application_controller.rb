6eaa623428d478a6fcea79afaddad0fcbdd72988
Christopher Guess <cguess@gmail.com>
2024-03-19 04:22:36 -0400

    Fix for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
     def verify_auth_key
       auth_key = params["auth_key"]
 
-      if auth_key.nil? || auth_key != Setting.auth_key
+      # if auth_key.nil? || auth_key != Setting.auth_key
+      if auth_key.nil? || auth_key != Figaro.env.PRESET_API_KEY
         render json: { error: "Unauthorized key" }, status: 401
         return false
       end
