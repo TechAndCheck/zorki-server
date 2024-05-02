115aced2f9e2b5154b190bd5a60bfd6df6f74f47
Christopher Guess <cguess@gmail.com>
2024-05-02 16:21:08 -0400

    added logging to checkin

--- config/initializers/grigori_checkin.rb
+++ config/initializers/grigori_checkin.rb
@@ -1,12 +1,13 @@
 # This will error out, but we can see it! so it's fine
-logger.info "Sending check-in to Grigori"
+Rails.logger.info "********************************************************************"
+Rails.logger.info "Sending check-in to Grigori"
 request = Typhoeus::Request.new("http://10.211.55.2:2345/tests_completed",
 headers: { "Content-Type": "application/json" },
 method: :post,
-body: { vm_id: ENV["VM_NAME"], status_code: status_code, status_message: status_message }.to_json)
+body: { vm_id: ENV["VM_NAME"], status_code: 100, status_message: "Successfully booted" }.to_json)
 
 request.run
 # response = request.response
 
-logger.info "Check-in sent"
-logger.debug(request.response)
+Rails.logger.info "Check-in sent. Response Code: #{request.response.code}"
+Rails.logger.info "********************************************************************"
