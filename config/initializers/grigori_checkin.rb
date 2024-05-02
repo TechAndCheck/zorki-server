2e7467281bc17b732770c78266e89549b0835581
Christopher Guess <cguess@gmail.com>
2024-05-02 17:53:20 -0400

    whoops

--- config/initializers/grigori_checkin.rb
+++ config/initializers/grigori_checkin.rb
@@ -3,7 +3,7 @@ Rails.logger.info "*************************************************************
 Rails.logger.info "Sending check-in to Grigori"
 request = Typhoeus::Request.new("https://10.211.55.2:4000/tests_completed",
 ssl_verifypeer: false, # This is to avoid SSL verification error
-ssl_verifyhost: false, # This is to avoid SSL verification error
+ssl_verifyhost: 0, # This is to avoid SSL verification error
 headers: { "Content-Type": "application/json" },
 method: :post,
 body: { vm_id: ENV["VM_NAME"], status_code: 100, status_message: "Successfully booted" }.to_json)
