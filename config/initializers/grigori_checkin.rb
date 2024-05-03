34714730a20ac502a5fd6e25ea958a296f2689c6
Christopher Guess <cguess@gmail.com>
2024-05-02 23:57:09 -0400

    update url

--- config/initializers/grigori_checkin.rb
+++ config/initializers/grigori_checkin.rb
@@ -1,13 +1,13 @@
 # This will error out, but we can see it! so it's fine
 Rails.logger.info "********************************************************************"
 Rails.logger.info "Sending check-in to Grigori"
-request = Typhoeus::Request.new("https://10.211.55.2:3000/tests_completed",
+request = Typhoeus::Request.new("https://10.211.55.2:3000/api/scraper/#{ENV["VM_NAME"]}/status_update",
 ssl_verifypeer: false, # This is to avoid SSL verification error
 ssl_verifyhost: 0, # This is to avoid SSL verification error since the self-signed cert is for 0.0.0.0
 timeout: 2, # This will be very quick normally, but when we're testing it'll hang for like 30 seconds otherwise
 headers: { "Content-Type": "application/json" },
 method: :post,
-body: { vm_id: ENV["VM_NAME"], status_code: 100, status_message: "Successfully booted" }.to_json)
+body: { vm_id: ENV["VM_NAME"], status: { code: 100, message: "Successfully booted" } }.to_json)
 
 request.run
 # response = request.response
