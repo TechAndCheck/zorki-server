c71879f127dfc98aec303953f4ca213cdc7366dd
Christopher Guess <cguess@gmail.com>
2024-05-09 14:22:14 -0400

    modify environment for ports

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.web_console.whitelisted_ips = "0.0.0.0/24"
+  config.web_console.whitelisted_ips = "0.0.0.0:5000"
+
+  # TODO: This needs to have a port range of some sort, maybe done in the start up script?
 end
