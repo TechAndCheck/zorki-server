7b683bbb73cf42da6f03612403a840d56f3c5233
Christopher Guess <cguess@gmail.com>
2024-05-09 17:17:26 -0400

    fix stuff

--- config/puma.rb
+++ config/puma.rb
@@ -43,4 +43,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "ssl://0.0.0.0:3000"
+# bind "ssl://0.0.0.0:3000"
+bind "tcp://0.0.0.0:3000"
