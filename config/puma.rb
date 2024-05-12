3c470641d6a76e0bcf1b47d0c6c4bbad148a75d7
Christopher Guess <cguess@gmail.com>
2024-05-12 14:27:50 -0400

    Bind Puma to SSL in production

--- config/puma.rb
+++ config/puma.rb
@@ -43,6 +43,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
+debugger
 if ENV["RAILS_ENV"] == "production" # This only runs in a VM with local access, so production this is good
   # Fail if in production but the keys don't exist
   certs_path = "/home/parallels/Desktop/Parallels\ Shared\ Folders/env_injection_files/ssl_certs"
