1451c5bbfdff4a17bc7a83d7f630528d2ae610ff
Christopher Guess <cguess@gmail.com>
2024-10-11 18:24:51 -0400

    fix up where we wouldn't return anything or log anything.

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -16,13 +16,22 @@ class ScraperController < ApplicationController
       retry_count ||= 0
       results = MediaSource.scrape(url, callback_id, force: force)
     rescue MediaSource::HostError => e
+      logger.error({ error: "MediaSource::HostError encountered while scraping", url: url, count: retry_count += 1 }.to_json)
       render json: e.to_response_structure, status: 400
       return
     rescue Net::ReadTimeout => error
-      print({ error: "Net::ReadTimeout encountered while scraping", url: url, count: retry_count += 1 }.to_json)
+      logger.error({ error: "Net::ReadTimeout encountered while scraping", url: url, count: retry_count += 1 }.to_json)
       sleep(30)
-      raise error if retry_count > 4
+      if retry_count > 4
+        logger.error({ error: "Net::ReadTimeout encountered while scraping", url: url, count: retry_count }.to_json)
+        render json: "Net::ReadTimeout: #{error.message}", status: 400
+        return
+      end
+
       retry
+    rescue StandardError => e
+      render json: { error: e.message }, status: 400
+      return
     end
 
     if params["force"] == "true" && Figaro.env.ALLOW_FORCE == "true"
