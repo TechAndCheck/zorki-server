ac7ca0d58a5120b1b5adb254f41cb4f623af714c
Christopher Guess <cguess@gmail.com>
2024-10-12 18:01:55 -0400

    Change submit to post from get

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   defaults format: :json do
-    get "scrape", to: "scraper#scrape", as: "scrape", constraints: lambda { |req| req.format == :json }
+    post "scrape", to: "scraper#scrape", as: "scrape", constraints: lambda { |req| req.format == :json }
     get "heartbeat", to: "scraper#heartbeat", as: "heartbeat"
   end
 end
